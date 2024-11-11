9a168b4e3442687179284144bf3b9388528cd79a
Yuxin Duan <48413978+DYnamoX513@users.noreply.github.com>
2024-11-11 13:46:46 +0800

    Add Trouble.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,4 +60,76 @@ return {
   },
 
   { "Civitasv/cmake-tools.nvim", ft = { "cmake", "cpp", "c" } },
+  {
+    "folke/trouble.nvim",
+    opts = {
+      -- preview = {
+      --   type = "float",
+      --   relative = "editor",
+      --   border = "rounded",
+      --   title = "Preview",
+      --   title_pos = "center",
+      --   position = { 0, -2 },
+      --   size = { width = 0.3, height = 0.3 },
+      --   zindex = 200,
+      -- },
+      modes = {
+        diagnostics = {
+          mode = "diagnostics",
+          preview = {
+            type = "float",
+            relative = "editor",
+            border = "rounded",
+            title = "Preview",
+            title_pos = "center",
+            position = { 0, -2 },
+            size = { width = 0.3, height = 0.3 },
+            zindex = 200,
+          },
+        },
+        symbols = {
+          mode = "lsp_document_symbols",
+          preview = {
+            type = "split",
+            relative = "win",
+            position = "bottom",
+            size = 0.3,
+          },
+        },
+      },
+    }, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>ts",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>tL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>tQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
 }
