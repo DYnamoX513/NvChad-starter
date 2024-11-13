4bb75970e0a45de49f5bc352420f6ab1baf5fb29
Yuxin Duan <48413978+DYnamoX513@users.noreply.github.com>
2024-11-13 10:10:11 +0800

    Truncated file path in status line

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -36,9 +36,27 @@ M.lsp = { signature = true }
 M.ui = {
   statusline = {
     theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
-    order = { "mode", "file", "git", "%=", "f", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "en", "cwd" },
+    order = { "mode", "file", "git", "%=", "ff", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "en", "cwd" },
     modules = {
       f = "%f",
+      ff = function()
+        local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+        local path = vim.api.nvim_buf_get_name(buf)
+        if path == "" then
+          return "Empty"
+        end
+
+        local relative_path = vim.fn.fnamemodify(path, ":.")
+
+        -- truncated
+        if #relative_path > 40 then
+          local filename = vim.fn.fnamemodify(path, ":t")
+          return "¶ " .. filename
+        end
+
+        return relative_path
+      end,
+
       en = function()
         local encode = vim.bo.fileencoding
         return "∾ " .. encode:upper() .. " "
