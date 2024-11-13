b3e660737d6338564b84a8128a73bb83d918a30a
Yuxin Duan <48413978+DYnamoX513@users.noreply.github.com>
2024-11-13 10:09:27 +0800

    Change harper_ls and ruff orders

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,13 +4,13 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- local servers = { "pyright", "ruff_lsp", "clangd" }
-local servers = { "rust_analyzer", "harper_ls" }
+local servers = { "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 local on_attach = nvlsp.on_attach
 local on_init = nvlsp.on_init
 local capabilities = nvlsp.capabilities
 
--- lsps with default config
+-- lsp with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -18,6 +18,16 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+lspconfig.ruff.setup {
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    -- Disable hover in favor of Pyright
+    client.server_capabilities.hoverProvider = false
+  end,
+  on_init = on_init,
+  capabilities = capabilities,
+}
 
 lspconfig.pyright.setup {
   on_attach = on_attach,
@@ -35,16 +45,6 @@ lspconfig.pyright.setup {
       },
     },
   },
-}
-
-lspconfig.ruff.setup {
-  on_attach = function(client, bufnr)
-    on_attach(client, bufnr)
-    -- Disable hover in favor of Pyright
-    client.server_capabilities.hoverProvider = false
-  end,
-  on_init = on_init,
-  capabilities = capabilities,
 }
 
 lspconfig.clangd.setup {
@@ -65,6 +65,9 @@ lspconfig.clangd.setup {
 }
 
 lspconfig.harper_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
   settings = {
     ["harper-ls"] = {
       linters = {
