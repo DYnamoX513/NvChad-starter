58a9a8249943fec525887713717a2406ca9093ec
Yuxin Duan <48413978+DYnamoX513@users.noreply.github.com>
2024-11-22 15:22:54 +0800

    Awesome format_on_save status line and dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,6 +14,11 @@ M.base46 = {
     ["@comment"] = { italic = true },
   },
   hl_add = {
+    St_seed = {
+      fg = "#806040",
+      -- see ~/.local/share/nvim/lazy/base46/lua/base46/integrations/statusline/vscode_colored.lua
+      bg = { "statusline_bg", 1 },
+    },
     TreesitterContextBottom = {
       -- bold = true,
       italic = true,
@@ -40,15 +45,66 @@ M.nvdash = {
     [[┃┃┏┓╋┣┓┓┏┓┏┓  ┣ ┃┏┏┓  ┃┃┃┏┓╋╋┏┓┏┓┏]],
     [[┛┗┗┛┗┛┗┗┛┗┗┫  ┗┛┗┛┗   ┛ ┗┗┻┗┗┗ ┛ ┛]],
     [[           ┛                      ]],
+    [[󰹣  Format on save enabled (Conform)]],
+    [[󱏾  Format on save disabled (Buffer)]],
+    [[󱏽  Format on save disabled (Global)]],
+    [[]],
+  },
+  buttons = {
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    { txt = "󱩮  Toggle buffer format_on_save", keys = "Spc t f", cmd = "FormatToggle!" },
+    { txt = "󱩭  Toggle global format_on_save", keys = "Spc t F", cmd = "FormatToggle" },
+
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local ms = math.floor(stats.startuptime) .. " ms"
+        return "󱟬  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+      end,
+      hl = "NvDashFooter",
+      no_gap = true,
+    },
+    { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
   },
 }
 
 M.lsp = { signature = true }
 
+local fos_status = function()
+  -- conform.nvim format_on_save status
+  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+  -- check buffer setting
+  if require("configs.conform").format_on_save(buf) then
+    return "󰹣 "
+  end
+
+  if vim.g.disable_autoformat then
+    return "󱏽 "
+  end
+
+  return "󱏾 "
+end
+
 M.ui = {
   statusline = {
     theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
-    order = { "mode", "file", "git", "%=", "ff", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "en", "cwd" },
+    order = {
+      "mode",
+      "file",
+      "git",
+      "%=",
+      "fos",
+      "ff",
+      "lsp_msg",
+      "%=",
+      "diagnostics",
+      "lsp",
+      "cursor",
+      "en",
+      "cwd",
+    },
     modules = {
       -- f = "%f",
       ff = function()
@@ -63,10 +119,14 @@ M.ui = {
         -- truncated
         if #relative_path > 40 then
           local filename = vim.fn.fnamemodify(path, ":t")
-          return "¶ " .. filename
+          return "..." .. filename
         end
 
         return relative_path
+      end,
+
+      fos = function()
+        return "%#St_seed#" .. fos_status() .. "%X" .. "%#StText#" -- reset
       end,
 
       en = function()
